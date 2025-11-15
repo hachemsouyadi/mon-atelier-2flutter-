fa42a8900e11b25b0ba984cfdbe9834c9a53fd69
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 04:52:02 -0500

    nouvelle modif

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
           child:  Column(
@@ -17,6 +17,19 @@ class MainApp extends StatelessWidget {
               Text('Hello World!', style: TextStyle(fontSize: 24)),
               Text('here its my first app', style: TextStyle(fontSize: 40)),
            Text('Hello World!'),
+           Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent,        
+                  borderRadius: BorderRadius.circular(22), 
+                ),
+                child: Text(
+                  'Boîte stylisée avec Container',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+
   ]
         ),
       ),
